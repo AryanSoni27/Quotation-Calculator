7e7cc02264a9fa770ecaf7c2fde066f4c400a73b
AryanSoni27 <aryansoni33635@gmail.com>
2025-02-20 16:49:27 +0530

    This is my first commit of this project
    # Conflicts:
    #	README.md

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quotation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Quotation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Customer"),
+            SizedBox(height: 5),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.person),
+                labelText: "Customer Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                contentPadding: EdgeInsets.all(10),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Date
+            Text("Date"),
+            SizedBox(height: 5),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.calendar_month),
+                labelText: "Date",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                contentPadding: EdgeInsets.all(10),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Text("Project"),
+            SizedBox(height: 5),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.shopping_bag_sharp),
+                labelText: "Project Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                contentPadding: EdgeInsets.all(10),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+
+  }
+}
