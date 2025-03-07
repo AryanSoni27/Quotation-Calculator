d3f66da15818a5ded1f824745bf1d1af8ae0d635
AryanSoni27 <aryansoni33635@gmail.com>
2025-03-07 17:59:46 +0530

    COnfigured the Edit button of clients list

--- lib/screens/clients.dart
+++ lib/screens/clients.dart
@@ -59,113 +59,114 @@ class ClientScreenState extends State<ClientScreen> {
       //     title: const Text("Clients")
       // ),
       body:
-          clients.isEmpty
-              ? Center(child: Text('No clients added yet'))
-              : ListView.builder(
-                padding: EdgeInsets.all(10),
-                itemCount: clients.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final client = clients[index];
-                  return Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    margin: EdgeInsets.symmetric(
-                      vertical: 8,
-                    ),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(16),
-                      // Padding inside the card
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        // Avatar background color
-                        child: Icon(
-                          Icons.person,
-                          color: Colors.white,
-                        ),
-                      ),
-                      title: Text(
-                        "${client.firstName} ${client.lastName}",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black87,
-                        ),
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Icon(Icons.phone, size: 16, color: Colors.green),
-                              SizedBox(width: 5),
-                              Text(
-                                client.mobileNumber,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 5),
-                          Row(
-                            children: [
-                              Icon(Icons.home, size: 16, color: Colors.blue),
-                              SizedBox(width: 5),
-                              Expanded(
-                                // Prevent overflow
-                                child: Text('${client.streetAddress}, ${client.city}, ${client.state}',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    color: Colors.black54,
-                                  ),
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 1, // Limit to one line
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // Edit Button
-                          IconButton(
-                            icon: Icon(Icons.edit, color: Colors.black),
-                            onPressed: () async {
-                              final updatedItem = await showClientForm(
-                                context,
-                                existingItem: clients[index],
-                              );
-                              if (updatedItem != null) {
-                                setState(() {
-                                  clients[index] = updatedItem;
-                                });
-                              }
-                            },
-                          ),
-
-                          //Delete Button
-                          IconButton(
-                            icon: Icon(Icons.delete, color: Colors.red),
-                            onPressed: () {
-                              setState(() {
-                                clients.removeAt(index);
-                                saveClientList(); // Save updated list
-                              });
-                            },
-
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+      clients.isEmpty
+          ? Center(child: Text('No clients added yet'))
+          : ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: clients.length,
+        itemBuilder: (BuildContext context, int index) {
+          final client = clients[index];
+          return Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+            ),
+            child: ListTile(
+              contentPadding: EdgeInsets.all(16),
+              // Padding inside the card
+              leading: CircleAvatar(
+                backgroundColor: Colors.blueAccent,
+                // Avatar background color
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
               ),
+              title: Text(
+                "${client.firstName} ${client.lastName}",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Icon(Icons.phone, size: 16, color: Colors.green),
+                      SizedBox(width: 5),
+                      Text(
+                        client.mobileNumber,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Icon(Icons.home, size: 16, color: Colors.blue),
+                      SizedBox(width: 5),
+                      Expanded(
+                        // Prevent overflow
+                        child: Text('${client.streetAddress}, ${client.city}, ${client.state}',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black54,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1, // Limit to one line
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Edit Button
+                  IconButton(
+                    icon: Icon(Icons.edit, color: Colors.black),
+                    onPressed: () async {
+                      // Pass the existing client data to showClientForm
+                      final updatedClient = await showClientForm(
+                        context,
+                        existingItem: clients[index],
+                      );
+                      if (updatedClient != null) {
+                        setState(() {
+                          clients[index] = updatedClient;
+                          saveClientList(); // Save after updating
+                        });
+                      }
+                    },
+                  ),
+
+                  //Delete Button
+                  IconButton(
+                    icon: Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {
+                      setState(() {
+                        clients.removeAt(index);
+                        saveClientList(); // Save updated list
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
       // FloatingActionButton at the bottom right
       floatingActionButton: FloatingActionButton(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
--- lib/widgets/bottom_popup_client_details.dart
+++ lib/widgets/bottom_popup_client_details.dart
@@ -4,12 +4,24 @@ import 'package:quotation/models/client_details.dart';
 import 'package:quotation/models/client_details.dart';
 
 Future<ClientDetails?> showClientForm(BuildContext context, {ClientDetails? existingItem}) async {
+  // Create controllers
   TextEditingController firstNameController = TextEditingController();
   TextEditingController lastNameController = TextEditingController();
   TextEditingController mobileNumberController = TextEditingController();
   TextEditingController streetAddressController = TextEditingController();
   TextEditingController cityController = TextEditingController();
   TextEditingController stateController = TextEditingController();
+
+  // Populate controllers with existing data if available
+  if (existingItem != null) {
+    firstNameController.text = existingItem.firstName;
+    lastNameController.text = existingItem.lastName;
+    mobileNumberController.text = existingItem.mobileNumber;
+    streetAddressController.text = existingItem.streetAddress;
+    cityController.text = existingItem.city;
+    stateController.text = existingItem.state;
+  }
+
   return showModalBottomSheet<ClientDetails>(
     context: context,
     isScrollControlled: true,
@@ -27,145 +39,144 @@ Future<ClientDetails?> showClientForm(BuildContext context, {ClientDetails? exis
                   top: 16,
                 ),
                 child: Wrap(
-                  children: [
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        //Title of Client Popup
-                        Center(
-                          child: Text(
-                            "Enter Client Details",
-                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-
-                        //First Name Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: firstNameController,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.person),
-                            prefixIconColor: Colors.blue,
-                            labelText: "First Name",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                    children: [
+                      Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //Title of Client Popup
+                            Center(
+                              child: Text(
+                                existingItem != null ? "Edit Client Details" : "Enter Client Details",
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                        ),
-                        SizedBox(height: 10),
+                            SizedBox(height: 10),
 
-                        //Last Name Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: lastNameController,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.person),
-                            prefixIconColor: Colors.blue,
-                            labelText: "Last Name",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            //First Name Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: firstNameController,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.person),
+                                prefixIconColor: Colors.blue,
+                                labelText: "First Name",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                        ),
-                        SizedBox(height: 10),
+                            SizedBox(height: 10),
 
-                        //Mobile Number Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: mobileNumberController,
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.phone),
-                            prefixIconColor: Colors.blue,
-                            labelText: "Mobile Number",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            //Last Name Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: lastNameController,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.person),
+                                prefixIconColor: Colors.blue,
+                                labelText: "Last Name",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                          inputFormatters: [
-                            LengthLimitingTextInputFormatter(10),
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                        ),
-                        SizedBox(height: 10),
-                        Text("Address:-"),
-                        //Street Address Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: streetAddressController,
-                          keyboardType: TextInputType.streetAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.home_filled),
-                            prefixIconColor: Colors.blue,
-                            labelText: "Street Address",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            SizedBox(height: 10),
+
+                            //Mobile Number Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: mobileNumberController,
+                              keyboardType: TextInputType.phone,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.phone),
+                                prefixIconColor: Colors.blue,
+                                labelText: "Mobile Number",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(10),
+                                FilteringTextInputFormatter.digitsOnly,
+                              ],
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-
-                        //City Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: cityController,
-                          keyboardType: TextInputType.streetAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.home_filled),
-                            prefixIconColor: Colors.blue,
-                            labelText: "City",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            SizedBox(height: 10),
+                            Text("Address:-"),
+                            //Street Address Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: streetAddressController,
+                              keyboardType: TextInputType.streetAddress,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.home_filled),
+                                prefixIconColor: Colors.blue,
+                                labelText: "Street Address",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                        ),
-                        SizedBox(height: 10),
+                            SizedBox(height: 10),
 
-                        //State Field
-                        TextField(
-                          textAlign: TextAlign.left,
-                          controller: stateController,
-                          keyboardType: TextInputType.streetAddress,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.home_filled),
-                            prefixIconColor: Colors.blue,
-                            labelText: "State",
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            //City Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: cityController,
+                              keyboardType: TextInputType.streetAddress,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.home_filled),
+                                prefixIconColor: Colors.blue,
+                                labelText: "City",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
                             ),
-                            contentPadding: EdgeInsets.all(10),
-                          ),
-                        ),
-                        SizedBox(height: 20),
+                            SizedBox(height: 10),
 
-                        //Add Client Button
-                        Center(
-                            child: ElevatedButton(
-                              onPressed: () {
-                                final client = ClientDetails(
-                                    firstName: firstNameController.text,
-                                    lastName: lastNameController.text,
-                                    mobileNumber: mobileNumberController.text,
-                                    streetAddress: streetAddressController.text,
-                                    city: cityController.text,
-                                    state: stateController.text,
-                                );
-                                Navigator.of(context).pop(client);
-                              },
-                              child: Text("Add Client"),
+                            //State Field
+                            TextField(
+                              textAlign: TextAlign.left,
+                              controller: stateController,
+                              keyboardType: TextInputType.streetAddress,
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.home_filled),
+                                prefixIconColor: Colors.blue,
+                                labelText: "State",
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                contentPadding: EdgeInsets.all(10),
+                              ),
+                            ),
+                            SizedBox(height: 20),
 
+                            //Add Client Button
+                            Center(
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    final client = ClientDetails(
+                                      firstName: firstNameController.text,
+                                      lastName: lastNameController.text,
+                                      mobileNumber: mobileNumberController.text,
+                                      streetAddress: streetAddressController.text,
+                                      city: cityController.text,
+                                      state: stateController.text,
+                                    );
+                                    Navigator.of(context).pop(client);
+                                  },
+                                  child: Text(existingItem != null ? "Update Client" : "Add Client"),
+                                )
                             )
-                        )
-                      ]
-                    )
-                  ]
+                          ]
+                      )
+                    ]
                 )
             );
           }
